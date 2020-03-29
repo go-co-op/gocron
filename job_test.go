bd904669f8c3e40fa4879e4ffae6b02a8d33c321
JohnRoesler <johnrroesler@gmail.com>
2020-03-28 22:57:57 -0500

    Update all exported comments and start example_test.go for godoc

--- job_test.go
+++ job_test.go
@@ -26,6 +26,30 @@ func TestGetAt(t *testing.T) {
 }
 
 func TestGetWeekday(t *testing.T) {
-	j, _ := NewScheduler(time.UTC).Every(1).Weekday(time.Wednesday).Do(task)
-	assert.Equal(t, time.Wednesday, j.GetWeekday())
+	s := NewScheduler(time.UTC)
+	weedayJob, _ := s.Every(1).Weekday(time.Wednesday).Do(task)
+	nonWeekdayJob, _ := s.Every(1).Minute().Do(task)
+
+	testCases := []struct {
+		desc            string
+		job             *Job
+		expectedWeekday time.Weekday
+		expectedError   error
+	}{
+		{"success", weedayJob, time.Wednesday, nil},
+		{"fail - not set for weekday", nonWeekdayJob, time.Sunday, ErrNotScheduledWeekday},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			weekday, err := tc.job.GetWeekday()
+			if tc.expectedError != nil {
+				assert.Error(t, tc.expectedError, err)
+			} else {
+				assert.Nil(t, err)
+			}
+
+			assert.Equal(t, tc.expectedWeekday, weekday)
+		})
+	}
 }
