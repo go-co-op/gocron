473485d273dc3d56771c93bcbd7a67243fca8e2a
streppel <streppels@gmail.com>
2020-04-10 17:34:14 -0300

    mvoes task to it's own function

--- example_test.go
+++ example_test.go
@@ -7,21 +7,25 @@ import (
 	"github.com/go-co-op/gocron"
 )
 
+var task = func() {
+	fmt.Println("I am a task")
+}
+
 func ExampleScheduler_Start() {
 	s := gocron.NewScheduler(time.UTC)
-	s.Every(3).Seconds().Do(func() { fmt.Println("I am a task") })
-	<-s.StartAsync()
+	s.Every(3).Seconds().Do(task)
+	s.StartBlocking()
 }
 
 func ExampleScheduler_At() {
 	s := gocron.NewScheduler(time.UTC)
-	s.Every(1).Day().At("10:30").Do(func() { fmt.Println("I am a task") })
-	s.Every(1).Monday().At("10:30:01").Do(func() { fmt.Println("I am a task") })
+	s.Every(1).Day().At("10:30").Do(task)
+	s.Every(1).Monday().At("10:30:01").Do(task)
 }
 
 func ExampleJob_GetScheduledTime() {
 	s := gocron.NewScheduler(time.UTC)
-	job, _ := s.Every(1).Day().At("10:30").Do(func() { fmt.Println("I am a task") })
+	job, _ := s.Every(1).Day().At("10:30").Do(task)
 	fmt.Println(job.GetScheduledTime())
 	// Output: 10:30
 }
