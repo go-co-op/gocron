dc27090f2bdced5f6c787ccc880d007ffb515920
John Roesler <johnrroesler@gmail.com>
2020-04-11 14:40:52 -0500

    go vet

--- example_test.go
+++ example_test.go
@@ -23,7 +23,7 @@ func ExampleScheduler_At() {
 	s.Every(1).Monday().At("10:30:01").Do(task)
 }
 
-func ExampleJob_GetScheduledTime() {
+func ExampleJob_ScheduledTime() {
 	s := gocron.NewScheduler(time.UTC)
 	job, _ := s.Every(1).Day().At("10:30").Do(task)
 	fmt.Println(job.ScheduledAtTime())
