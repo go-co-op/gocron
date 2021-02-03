ec0c59e029b7a207b742551bf4facf51ee615407
sdw2330976 <sydhappy123@163.com>
2021-02-04 00:26:56 +0800

    fix calculateMonths (#104)
    * when we use every month at some time,then calculate next run time use base time is not consistency
    
    * test calculateMonths
    
    * Apply suggestions from code review
    
    Co-authored-by: John Roesler <johnrroesler@gmail.com>
    
    Co-authored-by: shangyindong <shangyindong@p1.com>
    Co-authored-by: John Roesler <johnrroesler@gmail.com>

--- scheduler_test.go
+++ scheduler_test.go
@@ -988,3 +988,19 @@ func TestRemoveAfterExec(t *testing.T) {
 
 	assert.Zero(t, len(s.Jobs()))
 }
+
+func TestCalculateMonths(t *testing.T) {
+	ft := fakeTime{onNow: func(l *time.Location) time.Time {
+		return time.Date(1970, 1, 1, 12, 0, 0, 0, l)
+	}}
+	s := NewScheduler(time.UTC)
+	s.time = ft
+	s.StartAsync()
+	job, err := s.Every(1).Month(1).At("10:00").Do(func() {
+		fmt.Println("hello task")
+	})
+	require.NoError(t, err)
+	s.Stop()
+
+	assert.Equal(t, s.time.Now(s.location).AddDate(0, 1, 0).Month(), job.nextRun.Month())
+}
