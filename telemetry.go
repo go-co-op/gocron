38165da1197218d4297eedf05509b3d3a48f6b8b
Arjun Mahishi <arjun.mahishi@gmail.com>
2023-02-07 00:11:53 +0530

    telemetry: configurable latency buckets

--- telemetry.go
+++ telemetry.go
@@ -7,16 +7,22 @@ import (
 )
 
 var (
-	jobLatency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name: "gocron_job_latency_milliseconds",
-		Help: "time taken to execute a job in milliseconds",
-	}, []string{"job_name", "scheduler_name"})
-
+	jobLatency    *prometheus.HistogramVec
 	telemetryOnce sync.Once
+
+	// Latency buckets for job latency metrics. This can be overridden on the
+	// application side
+	LatencyBuckets = prometheus.ExponentialBuckets(1, 2.5, 15)
 )
 
 func initTelemetry() {
 	telemetryOnce.Do(func() {
+		jobLatency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Name:    "gocron_job_latency_milliseconds",
+			Help:    "time taken to execute a job in milliseconds",
+			Buckets: LatencyBuckets,
+		}, []string{"job_name", "scheduler_name"})
+
 		prometheus.MustRegister(jobLatency)
 	})
 }
