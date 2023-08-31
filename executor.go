0eaa280461fea0e6a149dff49f73affb21de1d3a
rfyiamcool <rfyiamcool@163.com>
2023-08-31 15:55:56 +0800

    fix: modify the boundary of max_concurrent_jobs

--- executor.go
+++ executor.go
@@ -222,7 +222,7 @@ func (e *executor) run() {
 				if e.limitModeMaxRunningJobs > 0 {
 					switch e.limitMode {
 					case RescheduleMode:
-						if e.limitModeRunningJobs.Load() < int64(e.limitModeMaxRunningJobs) {
+						if e.limitModeRunningJobs.Load() <= int64(e.limitModeMaxRunningJobs) {
 							select {
 							case e.limitModeQueue <- f:
 							case <-e.ctx.Done():
