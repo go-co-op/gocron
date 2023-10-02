9be305fe31d48072be28a67d7968d211d8e8d9b7
John Roesler <johnrroesler@gmail.com>
2023-10-02 07:25:07 -0500

    wip long running jobs

--- scheduler_test.go
+++ scheduler_test.go
@@ -8,102 +8,149 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestScheduler(t *testing.T) {
+func TestScheduler_OneSecond_NoOptions(t *testing.T) {
 	t.Parallel()
 
 	cronNoOptionsCh := make(chan struct{})
 	durationNoOptionsCh := make(chan struct{})
 
-	type testJob struct {
+	tests := []struct {
 		name string
 		ch   chan struct{}
 		jd   JobDefinition
-	}
-
-	tests := []struct {
-		name            string
-		testJobs        []testJob
-		options         []SchedulerOption
-		runCount        int
-		expectedTimeMin time.Duration
-		expectedTimeMax time.Duration
 	}{
 		{
-			"no scheduler options 1 second jobs",
-			[]testJob{
-				{
-					"cron",
-					cronNoOptionsCh,
-					CronJob(
-						"* * * * * *",
-						true,
-						Task{
-							Function: func() {
-								cronNoOptionsCh <- struct{}{}
-							},
-						},
-					),
+			"cron",
+			cronNoOptionsCh,
+			CronJob(
+				"* * * * * *",
+				true,
+				Task{
+					Function: func() {
+						cronNoOptionsCh <- struct{}{}
+					},
 				},
-				{
-					"duration",
-					durationNoOptionsCh,
-					DurationJob(
-						time.Second,
-						Task{
-							Function: func() {
-								durationNoOptionsCh <- struct{}{}
-							},
-						},
-					),
+			),
+		},
+		{
+			"duration",
+			durationNoOptionsCh,
+			DurationJob(
+				time.Second,
+				Task{
+					Function: func() {
+						durationNoOptionsCh <- struct{}{}
+					},
 				},
-			},
-			nil,
-
-			1,
-			time.Millisecond * 1,
-			time.Millisecond * 1500,
+			),
 		},
 	}
 
 	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 
-		for _, tj := range tt.testJobs {
-			t.Run(tt.name+"_"+tj.name, func(t *testing.T) {
-				t.Parallel()
+			s, err := NewScheduler()
+			require.NoError(t, err)
 
-				s, err := NewScheduler(tt.options...)
-				require.NoError(t, err)
+			_, err = s.NewJob(tt.jd)
+			require.NoError(t, err)
 
-				_, err = s.NewJob(tj.jd)
-				require.NoError(t, err)
+			s.Start()
 
-				s.Start()
+			startTime := time.Now()
+			var runCount int
+			for runCount < 1 {
+				<-tt.ch
+				runCount++
+			}
+			err = s.Stop()
+			require.NoError(t, err)
+			stopTime := time.Now()
 
-				startTime := time.Now()
-				var runCount int
-				for runCount < tt.runCount {
-					<-tj.ch
-					runCount++
-				}
-				err = s.Stop()
-				require.NoError(t, err)
-				stopTime := time.Now()
+			select {
+			case <-tt.ch:
+				t.Fatal("job ran after scheduler was stopped")
+			case <-time.After(time.Millisecond * 50):
+			}
 
-				select {
-				case <-tj.ch:
-					t.Fatal("job ran after scheduler was stopped")
-				case <-time.After(time.Millisecond * 50):
-				}
-
-				runDuration := stopTime.Sub(startTime)
-				assert.GreaterOrEqual(t, runDuration, tt.expectedTimeMin)
-				assert.LessOrEqual(t, runDuration, tt.expectedTimeMax)
-
-			})
-		}
+			runDuration := stopTime.Sub(startTime)
+			assert.GreaterOrEqual(t, runDuration, time.Millisecond)
+			assert.LessOrEqual(t, runDuration, 1500*time.Millisecond)
+		})
 	}
 }
 
-// TODO tests for singleton mode and other limit mode
-// need to handle case where some jobs are waiting for on shutdown
-// so can't use a channel because then the job is blocked trying to send
+func TestScheduler_LongRunningJobs(t *testing.T) {
+	t.Parallel()
+
+	durationCh := make(chan struct{}, 10)
+	durationSingletonCh := make(chan struct{}, 10)
+
+	tests := []struct {
+		name     string
+		ch       chan struct{}
+		jd       JobDefinition
+		options  []SchedulerOption
+		expected int
+	}{
+		{
+			"duration",
+			durationCh,
+			DurationJob(
+				time.Millisecond*500,
+				Task{
+					Function: func() {
+						time.Sleep(1 * time.Second)
+						durationCh <- struct{}{}
+					},
+				},
+			),
+			nil,
+			3,
+		},
+		{
+			"duration",
+			durationSingletonCh,
+			DurationJob(
+				time.Millisecond*500,
+				Task{
+					Function: func() {
+						time.Sleep(1 * time.Second)
+						durationSingletonCh <- struct{}{}
+					},
+				},
+				SingletonMode(),
+			),
+			nil,
+			2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			s, err := NewScheduler()
+			require.NoError(t, err)
+
+			_, err = s.NewJob(tt.jd)
+			require.NoError(t, err)
+
+			s.Start()
+			time.Sleep(1600 * time.Millisecond)
+			err = s.Stop()
+			require.NoError(t, err)
+
+			time.Sleep(1 * time.Second)
+			close(tt.ch)
+
+			var runCount int
+			for range tt.ch {
+				runCount++
+			}
+
+			assert.Equal(t, tt.expected, runCount)
+		})
+	}
+}
