1b7015ed65e1141defd8b4e463c94c0199e920ba
Derrick Hammer <derrick@derrickhammer.com>
2024-07-22 05:36:17 -0400

    feat: add lock update mechanism for jobs, closes #762
    - Add `jobOutUpdateLockRequest` channel in the executor
    - Implement lock update requests in the job execution process
    - Add `Lock()` method to the `Job` interface
    - Update the scheduler to handle lock update requests
    - Add a test case to verify the new locking mechanism

--- executor.go
+++ executor.go
@@ -401,6 +401,7 @@ func (e *executor) runJob(j internalJob, jIn jobIn) {
 
 		defer func() {
 			_ = lock.Unlock(j.ctx)
+
 		}()
 	} else if !j.disabledLocker && e.locker != nil {
 		lock, err := e.locker.Lock(j.ctx, j.name)
